3e3a131d4313ba2e5f7b6af585fbab8f4f7c510e
solyasss <olhastetsenko01@gmail.com>
2026-04-09 21:32:05 +0100

    feat: setup pages

---
+++ src/pages/AccountPage/AccountPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// TODO: implement AccountPage page
+</script>
+
+<template>
+  <div>AccountPage</div>
+</template>
---
+++ src/pages/CartPage/CartPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// TODO: implement CartPage page
+</script>
+
+<template>
+  <div>CartPage</div>
+</template>
